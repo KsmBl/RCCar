42f72680826af7b87b58446f11a442a1dddf5d85
KsmBl <katzen.sind.lecker69@gmail.com>
2024-09-03 13:55:13 +0000

    test first button

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #! /usr/bin/python
 
+import RPi.GPIO as GPIO
+from time import sleep
+
 #import ELRS lib
-#import PWM lib
 
 def main():
 	while (True):
 		setMotorSpeed(1500) #ELRS Axis 1
 		setSteer(1600) #ELRS Axis 3
+		if GPIO.input(8) == GPIO.HIGH:
+			print("Button was pushed!")
 
 def setMotorSpeed(speed):
-	print(f"Set Motor Speed to {speed}") #set PWM
+	pwm = speed - 1000
+	print(f"Set Motor PWM to {pwm}") #set PWM
 
 def setSteer(angle):
 	print(f"Set Steer angle to {angle}") #set PWM
