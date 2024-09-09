f1891f8b0ee69161407d76ac9584cd2a905787d1
KsmBl <katzen.sind.lecker69@gmail.com>
2024-09-09 12:01:45 +0000

    quality changes

--- setSteer.py
+++ setSteer.py
@@ -2,14 +2,17 @@
 #  0:	all fine
 #  1:	input out of possible range (1000 - 2000)
 
-def setSteer(pwm, PID):
-	if pwm < 980:
-		return 1
-	if pwm > 2020:
+def setSteer(pwm, PID = 0):
+	if not type(PID) == int:
+		PID = 0
+
+	# test if pwm signal is in valid range of ELRS axis
+	if pwm < 980 or pwm > 2020:
 		return 1
 
 	angle = PID + (pwm - 1500) / 5 # 1000-2000 -> -500-500 -> -100-100
 
+	#set angle to valid range
 	if angle < -100:
 		angle = -100
 	elif angle > 100:
--- test.py
+++ test.py
@@ -4,46 +4,64 @@ from setSteer import setSteer # local file
 from PIDloop import PIDloop # local file
 from alert import alert # local file
 
-### set Steer ###
-rt = setSteer(2500, 0)
-if rt == 0:
-	print(f"setSteer(2500, 0) ERROR! [{rt}]. Error code 1 expected")
 
-rt = setSteer(1500, 0)
-if not rt == 0:
-	print(f"setSteer(1500, 0) ERROR! [{rt}]. No Error code expected")
+def main():
+	rtvalue = 0
 
-rt = setSteer(500, 0)
-if rt == 0:
-	print(f"setSteer(500, 0) ERROR! [{rt}]. Error code 1 expected")
+	### set Steer ###
+	rt = setSteer(2500, 0)
+	if rt == 0:
+		print(f"setSteer(2500, 0) ERROR! [{rt}]. Error code 1 expected")
+		rtvalue = 1
 
-# PID overflow #
-rt = setSteer(1999, 50)
-if not rt == 0:
-	print(f"setSteer(1999, 50) ERROR! [{rt}]. No Error code expected")
+	rt = setSteer(1500, 0)
+	if not rt == 0:
+		print(f"setSteer(1500, 0) ERROR! [{rt}]. No Error code expected")
+		rtvalue = 1
 
-rt = setSteer(1000, -50)
-if not rt == 0:
-	print(f"setSteer(1000, -50) ERROR! [{rt}]. No Error code expected")
+	rt = setSteer(500, 0)
+	if rt == 0:
+		print(f"setSteer(500, 0) ERROR! [{rt}]. Error code 1 expected")
+		rtvalue = 1
 
-### set Speed ###
-rt = setSpeed(2500)
-if rt == 0:
-	print(f"setSpeed(2500) ERROR! [{rt}]. Error code 1 expected")
+	# PID overflow #
+	rt = setSteer(1999, 50)
+	if not rt == 0:
+		print(f"setSteer(1999, 50) ERROR! [{rt}]. No Error code expected")
+		rtvalue = 1
 
-rt = setSpeed(1500)
-if not rt == 0:
-	print(f"setSpeed(1500) ERROR! [{rt}]. No Error code expected")
+	rt = setSteer(1000, -50)
+	if not rt == 0:
+		print(f"setSteer(1000, -50) ERROR! [{rt}]. No Error code expected")
+		rtvalue = 1
 
-rt = setSpeed(500)
-if rt == 0:
-	print(f"setSpeed(500) ERROR! [{rt}]. Error code 1 expected")
+	### set Speed ###
+	rt = setSpeed(2500)
+	if rt == 0:
+		print(f"setSpeed(2500) ERROR! [{rt}]. Error code 1 expected")
+		rtvalue = 1
 
-### get Inputs ###
-rt = getInputs()
+	rt = setSpeed(1500)
+	if not rt == 0:
+		print(f"setSpeed(1500) ERROR! [{rt}]. No Error code expected")
+		rtvalue = 1
 
-for i in rt:
-	if i > 2010:
-		print(f"one value from getInput() is higher than 2010: {i}")
-	elif i < 980:
-		print(f"one value from getInput() is lower than 980: {i}")
+	rt = setSpeed(500)
+	if rt == 0:
+		print(f"setSpeed(500) ERROR! [{rt}]. Error code 1 expected")
+		rtvalue = 1
+
+	### get Inputs ###
+	rt = getInputs()
+
+	for i in rt:
+		if i > 2020:
+			print(f"one value from getInput() is higher than 2020: {i}")
+			rtvalue = 1
+		elif i < 980:
+			print(f"one value from getInput() is lower than 980: {i}")
+			rtvalue = 1
+
+	return rtvalue
+
+exit(main())
